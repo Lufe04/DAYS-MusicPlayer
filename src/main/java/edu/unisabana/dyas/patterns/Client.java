bb92f654d5e3acf140c1dfef1d9880f23dab69d7
Lufe04 <luisacarpintero01@gmail.com>
2024-08-19 11:36:22 -0500

    Se realiza el taller

--- src/main/java/edu/unisabana/dyas/patterns/Client.java
+++ src/main/java/edu/unisabana/dyas/patterns/Client.java
@@ -1,12 +1,11 @@
-package main.java.edu.unisabana.dyas.patterns;
-
-import main.java.edu.unisabana.dyas.patterns.utils.AdvancedAudioPlayer;
+package edu.unisabana.dyas.patterns;
+import edu.unisabana.dyas.patterns.utils.AdvancedAudioPlayerAdapater;
 
 // Cliente que utiliza directamente AdvancedAudioPlayer sin conocer la interfaz AudioPlayer
 public class Client {
     public static void main(String[] args) {
-        AdvancedAudioPlayer advancedPlayer = new AdvancedAudioPlayer();
-        advancedPlayer.playMp4("video.mp4");
-        advancedPlayer.stop();
+        AdvancedAudioPlayerAdapater advancedPlayerAdapter = new AdvancedAudioPlayerAdapater();
+        advancedPlayerAdapter.play("Mp4", "Cry");
+        advancedPlayerAdapter.stop();
     }
 }
---
+++ src/main/java/edu/unisabana/dyas/patterns/utils/AdvancedAudioPlayerAdapater.java
@@ -0,0 +1,29 @@
+package edu.unisabana.dyas.patterns.utils;
+
+import main.java.edu.unisabana.dyas.patterns.utils.AudioPlayer;
+
+public class AdvancedAudioPlayerAdapater implements AudioPlayer{
+    private final AdvancedAudioPlayer audio;
+    
+    public AdvancedAudioPlayerAdapater(){
+            System.out.println("Creando el adaptador de Advanced Audio Player");
+            this.audio = new AdvancedAudioPlayer();
+    }
+    
+    @Override
+    public void play(String audioType, String fileName) {
+        System.out.println("Musica Sonando");
+        if (audioType.equals("Mp4")){
+            this.audio.playMp4(fileName);  
+        } else if (audioType.equals("VLC")){
+            this.audio.playVlc(fileName);
+        }
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Musica Detenida");
+        this.audio.stop();
+    }
+    
+}
